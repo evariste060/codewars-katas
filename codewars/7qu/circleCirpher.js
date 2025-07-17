36ef88e4cf1d7ed5624a84021b538bfc6d212e1a
evariste nkurunziza <nkurunzizaevariste060@gmail.com>
2025-07-17 17:29:53 +1100

    solution of codewars katas of 6 and 7 qu

--- codewars/7qu/circleCirpher.js
+++ codewars/7qu/circleCirpher.js
@@ -2,11 +2,10 @@ function encode(s) {
   let result =[];
   arr= s.split('');
   for(let i=0;i<Math.floor(s.length/2);i++){
-    let x = arr[i];
-    result.push(x);
-    result.push(arr[s.length-(i+1)])
+    result.push( arr[i]);
+    result.push(arr[s.length-(i+1)]);
   }
   if (s.length%2!==0) result.push(arr[Math.floor(s.length-1)/2]);
   return result.join('');
 }
-console.log(encode('codewars'));
+console.log(encode('codewars'));//csordaew white
